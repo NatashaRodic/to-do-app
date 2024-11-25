acbafbd2f984b070230b2a93026e9f53390eb1a1
Natasha Rodic <natashawolt@gmail.com>
2024-11-25 16:48:52 -0500

    Create a path for update page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.home, name='home'),
     path('create/', views.create, name='create'),
-    path('<int:task_id>', views.details, name='details')
+    path('<int:task_id>', views.details, name='details'),
+    path('<int:task_id>/update', views.update, name='update')
 ]
