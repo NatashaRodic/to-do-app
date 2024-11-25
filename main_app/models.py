8a44d95159a5574f638be553236ee6578d458f94
Natasha Rodic <natashawolt@gmail.com>
2024-11-25 14:10:29 -0500

    Create a model Task

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100, help_text='Your task')
+    description = models.TextField(max_length=300, help_text='Description of the task')
+
+    def __str__(self):
+        return self.title
