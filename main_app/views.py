b8e42b72d7677b3f61c67f5ce209107b708233f2
Natasha Rodic <natashawolt@gmail.com>
2024-11-25 15:41:36 -0500

    Create a function for task creation

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def home(request):
@@ -8,5 +9,12 @@ def home(request):
         'tasks': tasks
     })
 
-def create (request):
-    
+def create(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todolist')  # Redirect to the task list after saving
+    else:
+        form = TaskForm()
+    return render(request, 'create.html', {'form': form})
