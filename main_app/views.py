68e3567444014bce20c18aacd03d63d115444da0
Natasha Rodic <natashawolt@gmail.com>
2024-11-25 16:57:44 -0500

    Create the update function

--- main_app/views.py
+++ main_app/views.py
@@ -21,3 +21,11 @@ def details(request, task_id):
     return render(request, 'details.html', {
         'task': task
     })
+
+def update(request, task_id):
+    task = Task.objects.get(id=task_id)
+    form = TaskForm(request.POST or None, instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('')
+    return render(request, 'update.html', {'form': form, 'task': task})
