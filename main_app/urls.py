54ab21dd25ce74d1af8b2acf6debcd7954ff34cc
Natasha Rodic <natashawolt@gmail.com>
2024-11-25 17:07:30 -0500

    Add delete path

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('<int:task_id>', views.details, name='details'),
     path('<int:task_id>/update', views.update, name='update')
+    path('<int:task_id>/delete', views.delete, name='delete')
 ]
